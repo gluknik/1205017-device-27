8c567352aa57281b2c0a2c54514b90e2b3cb5a78
gluknik <57038095+gluknik@users.noreply.github.com>
2020-02-23 19:04:04 +0200

    Added JS for contact modal

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@ var contactLink = document.querySelector(".contact-btn");
 var contactPopup = document.querySelector(".modal-contact");
 var popupClose = document.querySelector(".modal-close");
 
-var name = contactPopup.querySelector("[name=name]");
-var email = contactPopup.querySelector("[name=email]");
-var message = contactPopup.querySelector("[name=message]");
+var name = contactPopup.querySelector("[name=contact-name]");
+var email = contactPopup.querySelector("[name=contact-email]");
+var message = contactPopup.querySelector("[name=contact-message]");
 var form = contactPopup.querySelector("form");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("name");
+} catch (err) {
+    isStorageSupport = false;
+}
 
 contactLink.addEventListener("click", function(evt) {
     evt.preventDefault();
     contactPopup.classList.add("modal-show");
-    name.focus();
+
+    if (storage) {
+      name.value = storage;
+      contactPopup.querySelector("[name=contact-email]").focus();
+    } else {
+      contactPopup.querySelector("[name=contact-name]").focus();
+      // не понимаю почему name.focus() не сработал
+    }
 });
 
-close.addEventListener("click", function (evt) {
+popupClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     contactPopup.classList.remove("modal-show");
     contactPopup.classList.remove("modal-error");
@@ -26,19 +42,26 @@ form.addEventListener("submit", function (evt) {
 
 form.addEventListener("submit", function (evt) {
     if (!name.value || !email.value || !message.value) {
+      console.log("name = " + name.value);
+      console.log("email = " + email.value);
+      console.log("message = " + message.value);
       evt.preventDefault();
       contactPopup.classList.remove("modal-error");
       contactPopup.offsetWidth = contactPopup.offsetWidth;
       contactPopup.classList.add("modal-error");
       console.log("Необходимо заполнить все поля формы");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("name", name.value);
+      }
     }
 });
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (сontactPopup.classList.contains("modal-show")) {
-        contactPopup.classList.remove("modal-show");
+      if(document.querySelector(".modal-contact").classList.contains("modal-show")) {
+        // тут в условии я пытался обратиться к contactPopup вместо document.querySelector(".modal-contact"), но получил ошибку в консоли.        contactPopup.classList.remove("modal-show");
         contactPopup.classList.remove("modal-error");
       }
     }
